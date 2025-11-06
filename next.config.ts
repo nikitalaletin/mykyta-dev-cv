66638ef5de0b4d0e07713fd63cfb7ca36466422b
Mykyta <mykyta.lalietin@gbu.org>
2025-11-06 18:20:29 +0200

    fix deploy

--- next.config.ts
+++ next.config.ts
@@ -1,12 +1,9 @@
 import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
-  output: 'export',
-  distDir: 'build',
   images: {
     unoptimized: true,
   },
-  trailingSlash: true,
 };
 
 export default nextConfig;
